5430e1b78ea615f4fd4ca4992535c3eb36507b52
zhengyuan <zhengyuan@yuanjihongdeMacBook-Pro.local>
2023-12-06 16:45:18 -0500

    update gallery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const toggle = document.querySelector('.toggle')
-const navigation = document.querySelector('.navigation')
+const navigation = document.querySelector('.nav')
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('expanded')) {
+    event.target.classList.toggle('expanded');
+  } else {
+    var expandedImages = document.querySelectorAll('.expanded');
+    expandedImages.forEach(function(img) {
+      img.classList.remove('expanded');
+    });
+  }
+});
+
+// Expand view for images
+function expandImage(imageId) {
+  var img = document.getElementById(imageId);
+  img.classList.toggle('expanded');
+  event.stopPropagation();
+}
